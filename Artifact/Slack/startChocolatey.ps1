8207c5470ad2e3c3ef65248b2680b4f84b474042
Ins4n3 <ischeglovitov@gmail.com>
2016-02-21 22:38:28 +0300

    fix start chocolatey installer

--- Artifact/Slack/startChocolatey.ps1
+++ Artifact/Slack/startChocolatey.ps1
@@ -14,19 +14,13 @@
 $secPassword = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential("$env:COMPUTERNAME\$($username)", $secPassword)
 
-$command = $file = $PSScriptRoot + "\ChocolateyPackageInstaller.ps1"
+$command = $PSScriptRoot + "\ChocolateyPackageInstaller.ps1"
 
-Enable-PSRemoting -Force -SkipNetworkProfileCheck
+# Run Chocolatey as the artifactInstaller user
+Enable-PSRemoting –Force -SkipNetworkProfileCheck
 
 # Ensure that current process can run scripts. 
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force 
-
-# Work around a bug in the Squirrel installer
-Invoke-Command -Credential $credential -ComputerName $env:COMPUTERNAME -ScriptBlock 
-{
-    Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope Process -Force
-    New-Item -Path "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\TrayNotify"  -Type directory -Force -ErrorAction Continue
-}
 
 Invoke-Command -FilePath $command -Credential $credential -ComputerName $env:COMPUTERNAME -ArgumentList $packageList
 Disable-PSRemoting -Force
